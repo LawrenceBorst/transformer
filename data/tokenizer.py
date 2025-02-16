acc325f15c1f1f09cc20b399da7325b58d8cbcba
Lawrence Borst <laurens.s.borst@gail.com>
2025-02-16 01:40:00 +0100

    Add `shift_right` property to tokenizer

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -12,14 +12,29 @@ class Tokenizer:
     """
 
     _sp: spm.SentencePieceProcessor
+    _shift_right: bool
 
     def __init__(self, spm_folder: str) -> None:
         self._sp = spm.SentencePieceProcessor(
             model_file=os.path.join(spm_folder, "spm.model")
         )
+        self._shift_right = False
+
+    @property
+    def shift_right(self) -> bool:
+        return self._shift_right
+
+    @shift_right.setter
+    def shift_right(self, val: bool) -> None:
+        self._shift_right = val
 
     def encode(self, sentence: str) -> torch.IntTensor:
-        return torch.IntTensor(self._sp.encode(sentence))
-    
+        result: torch.IntTensor = torch.IntTensor(self._sp.encode(sentence))
+
+        if self._shift_right:
+            result = torch.cat([torch.IntTensor([1]), result])
+
+        return result
+
     def decode(self, sentence: torch.IntTensor) -> str:
         return self._sp.decode(sentence.tolist())
