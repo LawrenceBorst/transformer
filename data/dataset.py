a2887e785cf31aaa71e8a8919a408fa891e20ed2
Lawrence Borst <laurens.s.borst@gail.com>
2025-02-16 22:51:09 +0100

    Add `max_samples` to the dataset constructor
    This argument allows you to limit the number of dataset items

--- data/dataset.py
+++ data/dataset.py
@@ -1,18 +1,29 @@
+import sys
 from torch.utils.data import Dataset
 
 
 class TextData(Dataset[str]):
-    _data: list[str]
+    """
+    A class for getting text data
 
-    def __init__(self, dataset_path: str) -> None:
+    Args:
+        dataset_path (str): the path to the dataset
+        max_samples (int): the maximum number of samples, for limiting the size of the dataset
+    """
+
+    _data: list[str]
+    _max_samples: int
+
+    def __init__(self, dataset_path: str, max_samples: int = sys.maxsize) -> None:
         self._data = self._get_data(dataset_path)
+        self._max_samples = max_samples
 
     def _get_data(self, dataset_path: str) -> list[str]:
         with open(dataset_path, "r") as f:
             return [line.rstrip("\n") for line in f]
 
     def __len__(self) -> int:
-        return len(self._data)
+        return min(self._max_samples, len(self._data))
 
     def __getitem__(self, idx: int) -> str:
         return self._data[idx]
