ad8143ed91e69be71f9d25e0a7ac826d5714ac9f
Lawrence Borst <laurens.s.borst@gail.com>
2025-02-17 20:59:32 +0100

    Add setup
    Add seed and device setup

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,3 +1,4 @@
 from .engine import Engine
 from .optimiser import get_optimizer
 from .loss import get_loss_function
+from .setup import set_seed, get_device
---
+++ engine/setup.py
@@ -0,0 +1,21 @@
+import torch
+import random
+import numpy as np
+
+
+def set_seed(seed: int = 0) -> None:
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+
+
+def get_device() -> torch.device:
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
